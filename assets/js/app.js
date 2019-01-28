a00deffadd9642814b3166d31cbfbe0e53d811bd
Egan Lecocq <e.lecocq@hotmail.com>
2019-01-28 01:18:54 +0100

    Mise en place de Encore pour le CSS et JS

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+var $ = require('jquery');
+
+global.$ = global.jQuery = $;
+
+require('bootstrap');
+require('./bootstrap-datepicker.min.js');
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,9 +13,13 @@ class HomeController extends AbstractController {
     /**
      * @Route("/", name="homepage")
      */
-    public function home() {
+    public function home(AdRepository $adRepo, UserRepository $userRepo) {
 
-        return $this->render('home.html.twig');
+        return $this->render('home.html.twig', [
+
+            'ads'   =>  $adRepo->findBestAds(3),
+            'users' =>  $userRepo->findBestUsers(2)
+        ]);
     }
 }
 
